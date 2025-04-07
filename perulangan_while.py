688339920bca8a8a9ba6b1f8ad85a9e8631ee899
krisnaxyoga <yogakris.yk@gmail.com>
2025-04-07 11:13:34 +0700

    perulangan

---
+++ perulangan_while.py
@@ -0,0 +1,19 @@
+jawab = 'ya'
+hitung = 0
+
+while(jawab == 'ya'):
+    hitung += 1
+    jawab = input("Ulang lagi tidak? ")
+
+print(f"Total perulagan: {hitung}")
+
+# jawab = 'ya'
+# hitung = 0
+
+# while(True):
+#     hitung += 1
+#     jawab = input("Ulang lagi tidak? ")
+#     if jawab == 'tidak':
+#         break
+
+# print(f"Total perulagan: {hitung}")
